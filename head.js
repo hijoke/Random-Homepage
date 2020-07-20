0cfb59d79ee7752868e64cf3017e60a0296e2607
hijoke <xcxcxcxcxxcxcxcxcx@Gmail.com>
2020-07-20 15:08:09 +0800

    Update head.js

--- head.js
+++ head.js
@@ -77,7 +77,7 @@
 
 	function play() {
 	  var head = document.getElementById('head');
-	  var history = (0, _gen2.default)('Stay simple, stay naive.').history;
+	  var history = (0, _gen2.default)('All is vanity and a striving after wind.').history;
 	  isRunning = true;
 	  var i = 0;
 	  history.forEach(function (text, i) {
